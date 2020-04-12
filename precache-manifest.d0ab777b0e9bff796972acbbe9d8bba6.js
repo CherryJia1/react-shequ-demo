a6b35780d4e9aad2ba40880b963d02642bb1b897
CherryJia1 <2748401271@qq.com>
2020-04-13 00:24:07 +0800

    first

---
+++ precache-manifest.d0ab777b0e9bff796972acbbe9d8bba6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60a19504eae69fd306aa2a7fb90d178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7348df458f21967babbf",
+    "url": "/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "6056a3bd75370a9d6cbe",
+    "url": "/static/css/main.91811117.chunk.css"
+  },
+  {
+    "revision": "7348df458f21967babbf",
+    "url": "/static/js/2.48692acc.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.48692acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6056a3bd75370a9d6cbe",
+    "url": "/static/js/main.8e87f350.chunk.js"
+  },
+  {
+    "revision": "519aebee06439d088657",
+    "url": "/static/js/runtime-main.9008f3bb.js"
+  },
+  {
+    "revision": "e1ea82cb1c39656b925012efe60f22ea",
+    "url": "/static/media/vue.e1ea82cb.png"
+  }
+]);
