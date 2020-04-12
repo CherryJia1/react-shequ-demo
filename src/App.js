3f27828aadcc560c497a19ccc7d5e4b0ff71f112
CherryJia1 <2748401271@qq.com>
2020-04-13 01:18:53 +0800

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 
 function App() {
